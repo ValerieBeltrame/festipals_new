310870c1d8bd65fe2ae13bf8cf4a308e98ff6ea9
beltrame.valerie <beltrame.valerie@gmail.com>
2017-01-08 19:23:37 +0100

    sorting and filter functionality for actspage

--- festipals/src/components/ActsSorter.js
+++ festipals/src/components/ActsSorter.js
@@ -4,19 +4,21 @@ import '../css/ActsSorter.css';
 export default class ActsSorter extends Component {
   constructor() {
     super();
-    var defaultOrder = 'All days';
-    var defaultFilter ='All';
+    var defaultOrder = 'All';
+    var defaultFilter ='All days';
     this.state = {daysButtonsCollapsed: 'collapse', sortButtonsCollapsed: 'collapse', selectedOrder: defaultOrder, selectedFilter: defaultFilter};
   }
 
-  changeHandlerOrder(event) {
+  changeHandlerFilter(event) {
     var a = document.getElementById('orderSelect');
-    var newSelectedOrder = a.options[a.selectedIndex].text;
-    this.setState({selectedOrder:newSelectedOrder});
+    var newSelectedFilter = a.options[a.selectedIndex].text;
+    this.props.handleFilter(a.options[a.selectedIndex].value)
+    this.setState({ selectedFilter: newSelectedFilter });
   }
 
-  changeHandlerFilter(name) {
-    this.setState({selectedFilter: name});
+  changeHandlerOrder(name) {
+    this.props.handleSort(name);
+    this.setState({selectedOrder: name});
   }
 
   collapseDaysButtons(event) {
@@ -38,6 +40,8 @@ export default class ActsSorter extends Component {
   }
 
   collapseAll(event) {
+    this.props.handleFilter('All');
+    this.props.handleSort('DateUp');
     this.setState({daysButtonsCollapsed: 'collapse', sortButtonsCollapsed: 'collapse', selectedOrder: 'All days', selectedFilter: 'All'});
   }
 
@@ -51,7 +55,7 @@ export default class ActsSorter extends Component {
               href="#actsDaysButtons" aria-expanded="false" aria-controls="actsDays" onClick={this.collapseDaysButtons.bind(this)}>
               <span className="fa fa-calendar-o fa-3x" aria-hidden="true"></span>
               <h3>Days</h3>
-              <p className="filterCurrentOption">{this.state.selectedOrder}</p>
+              <p className="filterCurrentOption">{this.state.selectedFilter}</p>
             </button>
           </div>
           <div className="btn-group">
@@ -59,7 +63,7 @@ export default class ActsSorter extends Component {
             href="#actsSorterButtons" aria-expanded="false" aria-controls="actsSorterButtons" onClick={this.collapseSortButtons.bind(this)}>
               <span className="fa fa-clone fa-rotate-270 fa-3x" aria-hidden="true"></span>
               <h3>Filter by</h3>
-              <p className="filterCurrentOption">{this.state.selectedFilter}</p>
+              <p className="filterCurrentOption">{this.state.selectedOrder}</p>
             </button>
           </div>
         </div>
@@ -68,15 +72,15 @@ export default class ActsSorter extends Component {
           <div className="panel panel-default">
             <div className="panel-body">
               <h3>Select Days</h3>
-              <select onChange={this.changeHandlerOrder.bind(this)} id="orderSelect" className="form-control">
-                <option>All days</option>
-                    <option>Monday, 01/02/2017</option>
-                    <option>Tuesday 02/02/2017</option>
-                    <option>Wednesday 03/02/2017</option>
-                    <option>Thursday 04/02/2017</option>
-                    <option>Friday 05/02/2017</option>
-                    <option>Saturday 06/02/2017</option>
-                    <option>Sunday 07/02/2017</option>
+              <select onChange={this.changeHandlerFilter.bind(this)} id="orderSelect" className="form-control">
+                <option value="All">All days</option>
+                <option value="Jun24">Saturday, 24/06/2017</option>
+                <option value="Jun25">Sunday, 25/06/2017</option>
+                <option value="Jun26">Monday 26/06/2017</option>
+                <option value="Jun27">Tuesday 27/06/2017</option>
+                <option value="Jun28">Wednesday 28/06/2017</option>
+                <option value="Jun29">Thursday 29/06/2017</option>
+                <option value="Jun30">Friday 30/06/2017</option>
               </select>
             </div>
           </div>
@@ -85,25 +89,25 @@ export default class ActsSorter extends Component {
         <div className={this.state.sortButtonsCollapsed} id="actsSorterButtons">
           <div className="btn-group btn-group-justified" role="group" aria-label="...">
             <div className="btn-group" role="group">
-              <button type="button" className="btn btn-default" onClick={this.changeHandlerFilter.bind(this, 'Country')}>
+              <button type="button" className="btn btn-default" onClick={this.changeHandlerOrder.bind(this, 'Country')}>
                 <span className="fa fa-globe fa-3x" aria-hidden="true"></span>
                 <h3 className="hidden-xs">Country</h3>
               </button>
             </div>
             <div className="btn-group" role="group">
-              <button type="button" className="btn btn-default" onClick={this.changeHandlerFilter.bind(this, 'Stage')}>
+              <button type="button" className="btn btn-default" onClick={this.changeHandlerOrder.bind(this, 'Stage')}>
                 <span className="fa fa-home fa-3x" aria-hidden="true"></span>
                 <h3 className="hidden-xs">Stage</h3>
               </button>
             </div>
             <div className="btn-group" role="group">
-              <button type="button" className="btn btn-default" onClick={this.changeHandlerFilter.bind(this, 'Ascending')}>
+              <button type="button" className="btn btn-default" onClick={this.changeHandlerOrder.bind(this, 'Ascending')}>
                 <span className="fa fa-sort-alpha-asc  fa-3x" aria-hidden="true"></span>
                 <h3 className="hidden-xs">Ascending</h3>
               </button>
             </div>
             <div className="btn-group" role="group">
-              <button type="button" className="btn btn-default" onClick={this.changeHandlerFilter.bind(this, 'Descending')}>
+              <button type="button" className="btn btn-default" onClick={this.changeHandlerOrder.bind(this, 'Descending')}>
                 <span className="fa fa-sort-alpha-desc  fa-3x" aria-hidden="true"></span>
                 <h3 className="hidden-xs">Descending</h3>
               </button>
